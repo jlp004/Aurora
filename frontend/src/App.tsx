6534147650022e72c0e7849bde8f77a6edc35f1d
john <johnlphelan2004@gmail.com>
2025-02-11 14:07:56 -0600

    basic routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import './styles/App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginForm from './components/LoginForm'
+import Home from './components/Home'
 
 function App() {
   
   return (
-    <>
-      <LoginForm>Username</LoginForm>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginForm />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+      </Routes>
+    </Router>
   )
 }
 
--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,23 +1,27 @@
 import '../styles/LoginForm.css'
+import { useNavigate } from 'react-router-dom'
 import { ReactNode } from 'react'
-// @ts-ignore
 import { auth, provider, signInWithPopup } from '../services/firebase'
 
 interface Props {
     children?: ReactNode
 }
 
-const handleClick = async () => {
+const LoginForm = ({children}: Props) => {
+  const navigate = useNavigate();
+  
+  const handleClick = async () => {
     try {
         const result = await signInWithPopup(auth, provider)
         alert("Logged in successfully!")
+        navigate("/home")
     } catch (error) {
         console.error("Login failed: ", error)
         alert("Login failed.")
     }
-}
+  }
 
-const LoginForm = ({children}: Props) => {
+
   return( 
   <>
     <button id="login-btn" className="btn btn-class-primary" onClick={handleClick}>Login</button>
