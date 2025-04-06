906a1a34c40313046176c7d85559d0e338b69515
Lydia <lxma021@gmail.com>
2025-04-06 00:51:37 -0400

    Edit main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,41 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.tsx'
 
+import { ChakraProvider } from '@chakra-ui/react' // Will need 'npm i @chakra-ui/react@2 @emotion/react @emotion/styled framer-motion'
+import { extendTheme } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
+import { BrowserRouter } from 'react-router-dom'
+/** 
+* Additional installation
+* npm i @chakra-ui/react @emotion/react
+* npm i react-icons react-router-dom
+**/ 
+
+// (Lydia)
+// Set background theme (May not need this)
+const styles = {
+  global: (props: Record<string, any>) => ({
+    body: {
+      bg: mode("gray.100", "#000")(props),
+      color: mode("gray.800", "whiteAlpha.900")(props),
+    },
+  }), 
+};
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
+
+const theme = extendTheme({ config, styles })
+// (Lydia)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter> 
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
