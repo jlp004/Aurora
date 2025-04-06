231b2a4895b133bef0552244b693a340bfcbe8de
meganchacko <79389137+meganchacko@users.noreply.github.com>
2025-04-06 17:59:19 -0500

    Post in Home page

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,9 +1,13 @@
 import Header from "../components/Header";
+import Post from '../components/Post'
 
 const Home = () => {
   return (
     <>
       <Header />
+      <div className = "post-feed">
+        <Post /> {}
+      </div>
     </>
   );
 };
