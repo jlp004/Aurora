e0c2a41ee90709d6fa782ce8312aea3a6f5a7d7f
charitha <charitha.sarraju@gmail.com>
2025-04-06 01:16:36 -0500

    login page updated

--- src/app/LoginForm.tsx
+++ src/app/LoginForm.tsx
@@ -16,12 +16,68 @@ const LoginForm = () => {
         alert("Login failed.")
     }
   }
+//testing
 
 
-  return( 
-  <>
-    <button id="login-btn" className="btn btn-class-primary" onClick={handleFirebaseAuth}>Login</button>
-  </>)
+  // return( 
+  // <>
+  //   <button id="login-btn" className="btn btn-class-primary" onClick={handleFirebaseAuth}>Login</button>
+  // </>)
+  return (
+    
+    <div style={{ //this is for the background, to create the gradiant effect to make it look like northern lights/gradient looking
+      background: 'linear-gradient(135deg,rgb(26, 22, 78) 0%,rgb(122, 50, 124) 100%)',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      position: 'relative',
+      height: '100vh', //having the height and weight for the page, as well as the alignment
+      width: '100vw',
+      alignItems: 'center'
+    }}>
+      
+      <h1 style={{ //this is for the text box saying the web app name "Aurora"
+        marginBottom: '300px', //aligning it as wanted
+        marginLeft: '-40px',
+        fontSize: '6rem', //increased font size for the app name effect, below are just some texxt formatting stuff
+        textShadow: '0 2px 4px rgba(0,0,0,0.3)',
+        fontWeight: 'bold', 
+      }}>
+        Aurora
+      </h1>
+
+      <button //this button helps you login using your google account
+  id="login-btn" 
+  className="btn btn-class-primary" 
+  onClick={handleFirebaseAuth} //using firebase for the google account login
+  
+  style={{
+    position: 'absolute',
+    right: '600px', //positioning for the button
+    top: '300px',
+    width: '200px', // Wider button
+    padding: '12px 0' // Adjusted padding for width
+  }}
+>
+  Login 
+</button>
+
+<button //this button is for the create account page
+        id="createAccount-btn"
+        className="btn btn-create-Account"
+        //onClick={handleOtherAction}
+        style={{
+          position: 'absolute', //formatting the position of the button to align it with the login button
+          right: '600px',
+          top: '360px',
+          width: '200px', // Wider button
+          padding: '12px 0' // Adjusted padding for width
+        }}
+      >
+        Create Account
+      </button>
+    </div>
+  );
 }
 
-export default LoginForm
+export default LoginForm //exporting it
