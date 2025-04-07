0871fe28a19ea5a4e9283f02ca94562083f8ed62
hishitashah <hishita.shah@gmail.com>
2025-04-06 23:35:56 -0500

    added comments to user account page

--- src/app/AccountPage.tsx
+++ src/app/AccountPage.tsx
@@ -1,16 +1,19 @@
-//made user account page - Hishita Shah
+//made user account page - Hishita Shah hrs220004
 
 import React from 'react';
 import '../styles/AccountPage.css';
 import Header from "../components/Header";
 import "../styles/HamburgerMenu.css"
 const AccountPage = () => {
+    //dummy user details for now, when database is ready will get data from there
     const user = {
-        username: "user123!",
-        bio: "Lover of code and coffee ☕",
-        followers: 120,
+        username: "user123!", //the username the person chose
+        bio: "Lover of code and coffee ☕", //short bio
+        followers: 120, //will display users followers and following 
         following: 80,
         posts: [
+            //dummy images for now, when database is ready will get data from there. 
+            //will be displayed in a grid format
             "/images/accountPic1.png",
             "/images/accountPic2.png",
             "/images/accountPic3.png"
@@ -18,15 +21,22 @@ const AccountPage = () => {
     };
 
     return (
+        //account page will display the user profile picture, bio, followers, following and posts in a grid format
+        //header will be displayed on top of the page
     <div className="account-container">
         <Header />  
-
+        
         <div className="profile-header">
-            <img src="/images/profile-pic.jpg" alt="Profile" className="profile-pic" />
+            {/* Profile picture will be displayed here */}
+            {/* The profile picture will be a circular image */}
+            <img src="/images/profile-pic.jpg" alt="Profile" className="profile-pic" />            
             <div className="user-info">
                 <h2>{user.username}</h2>
                 <p>{user.bio}</p>
                 <div className="stats">
+                    {/* Displaying the number of posts, followers and following */}
+                    {/* The numbers will be bold */}
+                    {/* The text will be displayed in a row */}
                     <span><strong>{user.posts.length}</strong> Posts</span>
                     <span><strong>{user.followers}</strong> Followers</span>
                     <span><strong>{user.following}</strong> Following</span>
@@ -34,6 +44,9 @@ const AccountPage = () => {
             </div>
         </div>
 
+        {/* Displaying the posts in a grid format */}
+        {/* The posts will be displayed in a 3x3 grid */}
+        {/* The images will be responsive */}
         <div className="image-grid">
             {user.posts.map((post, index) => (
                 <img key={index} src={post} alt={`Post ${index + 1}`} className="post-img" />
