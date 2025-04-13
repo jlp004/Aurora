d6cdef6867ee20efa7d718b9302168f56e5fb27f
john <johnlphelan2004@gmail.com>
2025-04-13 18:27:27 -0500

    fixed @ pathing i think

--- src/app/api/Post/route.ts
+++ src/app/api/Post/route.ts
@@ -6,7 +6,7 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function GET(req: NextRequest) {
   try {
     const posts = await prisma.post.findMany()
-    return NextResponse.json({ message: "Post fetched: ", data: posts }, { status: 200 } )
+    return NextResponse.json({ message: "Posts fetched: ", data: posts }, { status: 200 } )
   }
   catch (error) {
     return NextResponse.json({ message: error instanceof Error ? error.message : "Failed to retrieve posts" }, { status: 500})
