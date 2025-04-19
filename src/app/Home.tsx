b0373ba8a8bab901986b422ce2aca35383d9c6ee
hishitashah <hishita.shah@gmail.com>
2025-04-19 00:37:59 -0500

    header is visible on home page and background is changed to purple gradient

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,14 +1,15 @@
 import Header from "../components/Header";
-import Post from '../components/Post'
+import Post from '../components/Post';
+import '../styles/Home.css';
 
 const Home = () => {
   return (
-    <>
+    <div className="home-container">
       <Header />
-      <div className = "post-feed">
-        <Post /> {}
+      <div className="post-feed">
+        <Post />
       </div>
-    </>
+    </div>
   );
 };
 
