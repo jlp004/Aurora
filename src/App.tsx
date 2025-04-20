1f8d7fe5e6a4fe351d13bcab4565e5a78b1c0a34
Akash Jayakumar <axj210088@utdallas.edu>
2025-04-19 19:51:34 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './styles/App.css'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-//Landing
+// Landing
 import Login from './app/landingPage/login'
 import Signup from './app/landingPage/signup';
 
@@ -13,22 +13,29 @@ import AccountPage from './app/AccountPage';
 import Settings from './app/Settings';
 import ChatsPage from './app/ChatsPage';
 
+// Import Navbar
+import Navbar from './components/Navbar';
+
 function App() {  
   return (
+    <Router>
+      {/* Render the Navbar, it will appear across all pages */}
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="/search" element={<Search />}></Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/logout" element={<Logout />} />
-
-        <Route path="*" element = {<PageNotFound />} />
-        <Route path="/account" element={<AccountPage/>}></Route>
-        <Route path="/settings" element={<Settings/>}/>
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/chats" element={<ChatsPage/> } />
+        <Route path="*" element={<PageNotFound />} />
+        <Route path="/account" element={<AccountPage />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/chats" element={<ChatsPage />} />
       </Routes>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
