6bffc045eeb7cc412d8b520834092b0becb89a1f
jlp004 <johnlphelan2004@gmail.com>
2025-04-20 21:31:32 -0700

    updated Home posts

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -8,11 +8,30 @@ const Home = () => {
       <Header />
       <div className="post-feed">
         <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        
+        <Post 
+          imageUrl="/images/img2.png"
+          caption="Every day is a new day. Try to smile!"
+          username="nature_lover"
+          timePosted="3 hours ago"
+        />
+        <Post 
+          imageUrl="/images/img4.png"
+          caption="Beautiful beach spotted on morning run!!"
+          username="coffee_addict"
+          timePosted="5 hours ago"
+        />
+        <Post 
+          imageUrl="/images/accountPic3.png"
+          caption="Weekend vibes with friends"
+          username="party_person"
+          timePosted="1 day ago"
+        />
+        <Post 
+          imageUrl="/images/img3.png"
+          caption="Exploring new places"
+          username="world_explorer"
+          timePosted="2 days ago"
+        />
       </div>
     </div>
   );
